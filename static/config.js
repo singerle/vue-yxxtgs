7496fd736305aa2d55beef062706c8c64fd0b8ac
lixin <1411341896@qq.com>
2019-08-30 18:43:31 +0800

    修改通知书显示位置

--- static/config.js
+++ static/config.js
@@ -3,7 +3,7 @@ const config = {
 	// url: 'http://192.168.0.56:8086/enroll/'
 	// url: 'http://222.179.45.246:8083/enroll/'
 	// url: 'http://192.168.0.140:8086/enroll/'
-	// url: 'http://zsyx.ctbu.edu.cn/enroll/'//工商
+	url: 'http://zsyx.ctbu.edu.cn/enroll/'//工商
 	// url:'http://www.yunhuakeji.com:10071/enroll/',
 	// url: 'http://202.202.144.153:8082/enroll/'
 	//url: 'http://yx.cqut.edu.cn/enroll/'
